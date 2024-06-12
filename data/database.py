5a7aa8d5c5797a89a30ce56a57b6b49fa52a2641
wangyao <wangyao.dream@hotmail.com>
2024-06-12 23:51:49 +0800

    add database support

--- data/database.py
+++ data/database.py
@@ -1,4 +1,29 @@
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///data.sqlite3', echo=True)
+Base = declarative_base()
+
+
+class Messages(Base):
+    __tablename__ = 'messages'
+
+    name = Column(String, primary_key=True)
+    content = Column(String)
+
+
+# 创建所有表
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+
+
 
 
 
